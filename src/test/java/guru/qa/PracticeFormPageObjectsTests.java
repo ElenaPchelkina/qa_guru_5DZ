d74583362ad63bf860c1b40fed28576eebbf24f0
ElenaPchelkina <56049246+ElenaPchelkina@users.noreply.github.com>
2021-07-18 22:24:40 +0300

    Added testImplementation "com.github.javafaker:javafaker:1.0.2"

--- src/test/java/guru/qa/PracticeFormPageObjectsTests.java
+++ src/test/java/guru/qa/PracticeFormPageObjectsTests.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import pages.RegistrationPage;
 
 import java.io.File;
 
@@ -11,9 +12,17 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static org.graalvm.compiler.nodeinfo.InputType.State;
 
 
 public class PracticeFormPageObjectsTests {
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    Faker faker = new Faker();
+
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String email = faker.internet().emailAddress();
 
     @BeforeAll
     static void setup() {
@@ -21,47 +30,35 @@ public class PracticeFormPageObjectsTests {
         Configuration.startMaximized = true;
     }
 
-
     @Test
     void positiveFillTest(){
-        open("/automation-practice-form");
-        $("#firstName").setValue("Ivan");
-        $("#lastName").setValue("Ivanov");
-        $("#userEmail").setValue("1@ya.ru");
-        $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue("9250750048");
-        $("#dateOfBirthInput").click();
-        $(byClassName("react-datepicker__month-select")).selectOption("April");
-        $(byClassName("react-datepicker__year-select")).selectOption("1988");
-        $(byClassName("react-datepicker__day--012")).click();
-        $("#subjectsInput").setValue("History").pressEnter();
-        $("#hobbiesWrapper").$(byText("Reading")).click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/test.jpg"));
-        $("#currentAddress"). setValue("My address");
-        $("#react-select-3-input").setValue("NCR").pressEnter();
-        $("#react-select-4-input").setValue("Noida").pressEnter();
-        $(byText("Submit")).scrollTo().click();
+        registrationPage.openPage()
+                .typeFirstName(firstName)
+                .typeLastName("Ivanov")
+                .typeEmail("1@ya.ru")
+                .selectGender("Male")
+                .typePhone("9250750048")
+                .setDateOfBirth("12", "April", "1988")
+                .typeSubjects("History")
+                .selectHobbies("Reading")
+                .uploadPicture()
+                .typeCurrentAddress("My address")
+                .selectState("NCR")
+                .selectCity("Noida")
+                .submitPage()
 
-        $(byClassName("modal-body")).shouldHave(text("Ivan"));
-        $(byClassName("modal-body")).shouldHave(text("Ivanov"));
-        $(byClassName("modal-body")).shouldHave(text("1@ya.ru"));
-        $(byClassName("modal-body")).shouldHave(text("Male"));
-        $(byClassName("modal-body")).shouldHave(text("9250750048"));
-        $(byClassName("modal-body")).shouldHave(text("12 April,1988"));
-        $(byClassName("modal-body")).shouldHave(text("History"));
-        $(byClassName("modal-body")).shouldHave(text("Reading"));
-        $(byClassName("modal-body")).shouldHave(text("test.jpg"));
-        $(byClassName("modal-body")).shouldHave(text("My address"));
-        $(byClassName("modal-body")).shouldHave(text("NCR"));
-        $(byClassName("modal-body")).shouldHave(text("Noida"));
-
-
-
-
-
-
-
-
+                .checkResultsValue("Ivan")
+                .checkResultsValue("Ivanov")
+                .checkResultsValue("1@ya.ru")
+                .checkResultsValue("Male")
+                .checkResultsValue("9250750048")
+                .checkResultsValue("12 April,1988")
+                .checkResultsValue("History")
+                .checkResultsValue("Reading")
+                .checkResultsValue("test.jpg")
+                .checkResultsValue("My address")
+                .checkResultsValue("NCR")
+                .checkResultsValue("Noida");
     }
 
 }
